a0d95be4fba8278339cee28c997bd79c775be554
Andrew Ekstedt <andrew.ekstedt@gmail.com>
2015-01-08 15:09:38 -0800

    test: Rename some variables.

--- keccak_test.go
+++ keccak_test.go
@@ -9,51 +9,51 @@ import (
 )
 
 var tests = []struct {
-	f      func() hash.Hash
-	name   string
-	text   string
-	vector string
+	f    func() hash.Hash
+	name string
+	text string
+	hash string
 }{
 	{
-		f:      newKeccak256,
-		name:   "Keccak-256",
-		vector: "c5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470",
+		f:    newKeccak256,
+		name: "Keccak-256",
+		hash: "c5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470",
 	},
 	{
-		f:      newKeccak512,
-		name:   "Keccak-512",
-		vector: "0eab42de4c3ceb9235fc91acffe746b29c29a8c366b7c60e4e67c466f36a4304c00fa9caf9d87976ba469bcbe06713b435f091ef2769fb160cdab33d3670680e",
+		f:    newKeccak512,
+		name: "Keccak-512",
+		hash: "0eab42de4c3ceb9235fc91acffe746b29c29a8c366b7c60e4e67c466f36a4304c00fa9caf9d87976ba469bcbe06713b435f091ef2769fb160cdab33d3670680e",
 	},
 	{
-		f:      New256,
-		name:   "SHA3-256",
-		vector: "a7ffc6f8bf1ed76651c14756a061d662f580ff4de43b49fa82d80a4b80f8434a",
+		f:    New256,
+		name: "SHA3-256",
+		hash: "a7ffc6f8bf1ed76651c14756a061d662f580ff4de43b49fa82d80a4b80f8434a",
 	},
 	{
-		f:      New512,
-		name:   "SHA3-512",
-		vector: "a69f73cca23a9ac5c8b567dc185a756e97c982164fe25859e0d1dcc1475c80a615b2123af1f5f94c11e3e9402c3ac558f500199d95b6d3e301758586281dcd26",
+		f:    New512,
+		name: "SHA3-512",
+		hash: "a69f73cca23a9ac5c8b567dc185a756e97c982164fe25859e0d1dcc1475c80a615b2123af1f5f94c11e3e9402c3ac558f500199d95b6d3e301758586281dcd26",
 	},
 	{
-		f:      New256,
-		name:   "SHA3-256",
-		text:   "The quick brown fox jumps over the lazy dog",
-		vector: "69070dda01975c8c120c3aada1b282394e7f032fa9cf32f4cb2259a0897dfc04",
+		f:    New256,
+		name: "SHA3-256",
+		text: "The quick brown fox jumps over the lazy dog",
+		hash: "69070dda01975c8c120c3aada1b282394e7f032fa9cf32f4cb2259a0897dfc04",
 	},
 }
 
 func TestHash(t *testing.T) {
 	for _, tt := range tests {
-		vector, err := hex.DecodeString(tt.vector)
+		want, err := hex.DecodeString(tt.hash)
 		if err != nil {
 			t.Errorf("%s(%q): %s", tt.name, tt.text, err)
 			continue
 		}
 		h := tt.f()
 		io.WriteString(h, tt.text)
-		sum := h.Sum(nil)
-		if !bytes.Equal(sum, vector) {
-			t.Errorf("%s(%q): want %x, got %x", tt.name, tt.text, vector, sum)
+		got := h.Sum(nil)
+		if !bytes.Equal(got, want) {
+			t.Errorf("%s(%q) = %x, want %x", tt.name, tt.text, got, want)
 		}
 	}
 }
